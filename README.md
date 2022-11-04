40780c3f31dc8a1117489ee36c9a5cc651ae6d8c
unistart <2604249649@qq.com>
2022-11-04 19:02:36 +0800

    配置完成

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-个人Neovim配置
+# 个人Neovim配置
+
+## 参考
+- [Neovim IDE from Scratch](https://www.youtube.com/watch?v=ctH-a-1eUME&t=11s "Youtube - 
+chris@machine")
+- 一键式配置: [Nvchad](https://nvchad.com/)
+- Neovim版本管理工具：[bob
+](https://github.com/MordechaiHadad/bob)
+
+## ⚠️注意
+- 使用[packer.nvim](https://github.com/wbthomason/packer.nvim)作为Neovim插件管理工具
+- Neovim安装插件时出现git超时问题解决
+	1. 使用代理确
+	2. 修改packe.nvim使用的镜像，参考[issue](https://github.com/nshen/learn-neovim-lua/issues/18)
+
--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -12,6 +12,8 @@ dashboard.section.header.val = {
 	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
 	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
+
+
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
@@ -28,7 +30,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return ""
 end
 
 dashboard.section.footer.val = footer()
